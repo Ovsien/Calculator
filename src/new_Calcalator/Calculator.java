dccb5320c62feb57650b5c9da731299a26f286d1
Alexandr Ovsienko <ovsien91@gmail.com>
2020-09-30 21:53:16 +0700

    Созда лкласс Калькулятор, который работает с арабскими цифрами

--- src/new_Calcalator/Calculator.java
+++ src/new_Calcalator/Calculator.java
@@ -13,7 +13,8 @@ public class Calculator {
 
         List<StringValidator> list = new ArrayList<>();
         list.add(new StringValidator("^(\\d+)\\s*([+\\-*/])\\s*(\\d+)$", new ArabicHandler()));
-        list.add(new StringValidator("^([IVXLCDM]+)\\s*([+\\-*/])\\s*([IVXLCDM]+)$", new RomanHandler()));
+//        list.add(new StringValidator("^([IVXLCDM]+)\\s*([+\\-*/])\\s*([IVXLCDM]+)$", new RomanHandler()));
+        list.add(new StringValidator("(m/\\b((?:M{0,3}?(?:D?C{0,3}|C[DM])?(?:L?X{0,3}|X[LC])?(?:I{0,3}?V?I{0,3}|I[VX])))\\b/gs.)\\s*([+\\-*/])\\s*(m/\\b((?:M{0,3}?(?:D?C{0,3}|C[DM])?(?:L?X{0,3}|X[LC])?(?:I{0,3}?V?I{0,3}|I[VX])))\\b/gs.)$", new RomanHandler()));
 
         StringValidator validator = null;
 
